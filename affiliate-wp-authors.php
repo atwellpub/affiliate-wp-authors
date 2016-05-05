51959ce80ea3266f18067806e318213aa7cfc271
GITUSER <hudson@inboundnow.com>
2016-05-05 18:22:36 -0500

    tidy code

--- affiliate-wp-authors.php
+++ affiliate-wp-authors.php
@@ -9,72 +9,73 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 class AFFWP_Authors {
 
-	/**
-	*  Initialize class
-	*/
-	public function __construct() {
-		self::define_constants();
-		self::load_hooks();
-	}
+    /**
+     *  Initialize class
+     */
+    public function __construct() {
+        self::define_constants();
+        self::load_hooks();
+    }
 
-	/**
-	*  Define constants
-	*/
-	public static function define_constants() {
-		define('AFFWP_AUTHORS_CURRENT_VERSION', '1.0.1' );
-		define('AFFWP_AUTHORS_FILE' , __FILE__ );
-		define('AFFWP_AUTHORS_PATH', realpath(dirname(__FILE__)) . '/');
-		define('AFFWP_AUTHORS_URLPATH', WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' );
-	}
-	/**
-	*  Load hooks and filters
-	*/
-	public static function load_hooks() {
-		
-		/* Cookie Author */ 
+    /**
+     *  Define constants
+     */
+    public static function define_constants() {
+        define('AFFWP_AUTHORS_CURRENT_VERSION', '1.0.1');
+        define('AFFWP_AUTHORS_FILE', __FILE__);
+        define('AFFWP_AUTHORS_PATH', realpath(dirname(__FILE__)) . '/');
+        define('AFFWP_AUTHORS_URLPATH', WP_PLUGIN_URL . '/' . plugin_basename(dirname(__FILE__)) . '/');
+    }
+
+    /**
+     *  Load hooks and filters
+     */
+    public static function load_hooks() {
+
+        /* Cookie Author */
         add_action('wp_head', array(__CLASS__, 'cookie_author'));
 
-	}
-	
-	public static function cookie_author() {
+    }
 
-		$author_id = get_the_author_id();
+    public static function cookie_author() {
 
-		/* if author not set then bail */
-		if (!$author_id) {
-			return;
-		}
+        $author_id = get_the_author_id();
 
-		/* get affiliate id from author id */
-		$affiliate_id = affwp_get_affiliate_id( $author_id );
+        /* if author not set then bail */
+        if (!$author_id) {
+            return;
+        }
 
-		/* do not cookie internal traffic */
-		if (preg_match('/inboundnow.com/', $_SERVER['HTTP_REFERER'])) {
-			return;
-		}
+        /* get affiliate id from author id */
+        $affiliate_id = affwp_get_affiliate_id($author_id);
 
-		/* do not cookie rules */
-		if (
-			/* do not cookie for authors not registered as affiliates */
-			!$affiliate_id
-			||
-			/* do not cookie the cookied */
-			isset($_COOKIE['ref_cookie'])
-			||
-			/* do not cookie if we detect visitor is already a lead */
-			isset($_COOKIE['wp_lead_id'])
-		) {
-			return;
-		}
-		
-		setcookie('ref_cookie' , $affiliate_id , time() + ( 30 * 24 * 60 * 60),'/');
-		setcookie('affwp_campaign' , 'guest-publishing' , time() + ( 30 * 24 * 60 * 60) , '/');
+        /* do not cookie internal traffic */
+        if (preg_match('/inboundnow.com/', $_SERVER['HTTP_REFERER'])) {
+            return;
+        }
 
-	}
+        /* do not cookie rules */
+        if (
+            /* do not cookie for authors not registered as affiliates */
+            !$affiliate_id
+            ||
+            /* do not cookie the cookied */
+            isset($_COOKIE['ref_cookie'])
+            ||
+            /* do not cookie if we detect visitor is already a lead */
+            isset($_COOKIE['wp_lead_id'])
+        ) {
+            return;
+        }
+
+        setcookie('ref_cookie', $affiliate_id, time() + (30 * 24 * 60 * 60), '/');
+        setcookie('affwp_campaign', 'guest-publishing', time() + (30 * 24 * 60 * 60), '/');
+
+    }
 }
 
 new AFFWP_Authors;
